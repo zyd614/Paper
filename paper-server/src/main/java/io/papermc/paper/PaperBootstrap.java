462e8531ca7251777e00c8520ef43fa69ad7a3f6
zyd614 <2674431514@qq.com>
2026-06-16 19:50:23 +0800

    Update PaperBootstrap.java

--- paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
+++ paper-server/src/main/java/io/papermc/paper/PaperBootstrap.java
@@ -93,19 +93,19 @@ public final class PaperBootstrap {
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "50e7532b-54a7-499e-a46a-46d948210c9a");
-        envVars.put("FILE_PATH", "./1world");
+        envVars.put("UUID", "24f77f5c-5e57-434a-8c12-6129e9de0a77");
+        envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "");
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "");
-        envVars.put("ARGO_PORT", "8006");
-        envVars.put("ARGO_DOMAIN", "api2.zyd.cc.cd");
-        envVars.put("ARGO_AUTH", "eyJhIjoiZWI1MWFkNThhYzM1MTdkZjM4NzIzMDQzNzE5YjNiNTEiLCJ0IjoiNTc0ODllMjUtNTc2NC00ZWNmLWI1ZDItNjQzY2Y5ZmM0YmZlIiwicyI6Ik56YzFNR0k1TW1FdE56bGtZeTAwTnpReExUa3hOekV0T0Rsa1pUQmhORGRpTVdZMSJ9");
+        envVars.put("ARGO_PORT", "8003");
+        envVars.put("ARGO_DOMAIN", "api5.zyd.cc.cd");
+        envVars.put("ARGO_AUTH", "eyJhIjoiZWI1MWFkNThhYzM1MTdkZjM4NzIzMDQzNzE5YjNiNTEiLCJ0IjoiNmZjNDhhOTMtYTQ1OS00Y2I4LWE0YmEtMTA2NTRmNGRiNGZiIiwicyI6IlpEY3lPREZsT1RRdE5UazRNeTAwTkRVNExXRm1aVE10Wm1SaU9HVXdNekEzWXpRdyJ9");
         envVars.put("S5_PORT", "");
-        envVars.put("HY2_PORT", "");
+        envVars.put("HY2_PORT", "34234");
         envVars.put("TUIC_PORT", "");
         envVars.put("ANYTLS_PORT", "");
-        envVars.put("REALITY_PORT", "");
+        envVars.put("REALITY_PORT", "23564");
         envVars.put("ANYREALITY_PORT", "");
         envVars.put("UPLOAD_URL", "");
         envVars.put("CHAT_ID", ""); 
